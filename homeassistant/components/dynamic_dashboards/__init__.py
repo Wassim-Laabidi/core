4129e80dda0f08cc640c65686ec5d697a66f021d
Wassim Laabidi <laabidi.wassim250@gmail.com>
2024-07-30 15:01:08 +0100

    turn it back to user_id

--- homeassistant/components/dynamic_dashboards/__init__.py
+++ homeassistant/components/dynamic_dashboards/__init__.py
@@ -7,7 +7,7 @@ DOMAIN = "dynamic_dashboards"
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     async def handle_get_dashboard(call):
         user_id = call.context.user_id
-        dashboard_url = f"/lovelace-{{user}}/default_view"
+        dashboard_url = f"/lovelace-{user_id}/default_view"
         await hass.async_add_executor_job(
             hass.states.set,
             f"{DOMAIN}.dashboard_url",
