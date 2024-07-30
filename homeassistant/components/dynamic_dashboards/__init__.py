dad070124f4dec6984c8846e9838faa4559301fa
Wassim Laabidi <laabidi.wassim250@gmail.com>
2024-07-30 13:07:43 +0100

    changed __init__py

--- homeassistant/components/dynamic_dashboards/__init__.py
+++ homeassistant/components/dynamic_dashboards/__init__.py
@@ -1,15 +1,18 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers import service
 
 DOMAIN = "dynamic_dashboards"
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     async def handle_get_dashboard(call):
-        user = call.context.user_id
-        # Logic to determine the dashboard for the user
-        # For example, return a URL based on user ID
-        dashboard_url = f"/lovelace-{user}/default_view"
-        hass.states.set(f"{DOMAIN}.dashboard_url", dashboard_url)
+        user_id = call.context.user_id
+        dashboard_url = f"/lovelace-{user_id}/default_view"
+        await hass.async_add_executor_job(
+            hass.states.set,
+            f"{DOMAIN}.dashboard_url",
+            dashboard_url
+        )
 
     hass.services.async_register(DOMAIN, "get_dashboard", handle_get_dashboard)
     return True
